be37005a12af456a1bb792d05ae828dbae2a580c
Rob Bianca <rjbianca@hotmail.com>
2018-07-10 04:24:32 -0400

    Initial Commit

---
+++ Notepad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Notepad
+//
+//  Created by Rob Bianca on 7/10/18.
+//  Copyright © 2018 Robert Bianca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
